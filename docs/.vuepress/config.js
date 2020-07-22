f25051e7dfad459dacb2adebefbcee6e6461a513
why <1224125412@qq.com>
2020-07-22 21:26:08 +0800

    test rm

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,7 +18,8 @@ module.exports = {
     head: [
         ['meta', { name: 'theme-color', content: '#3eaf7c' }],
         ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
     ],
 
     /**
@@ -33,8 +34,10 @@ module.exports = {
     evergreen: true,
     serviceWorker: true,
     locales: {
-        '/': {
-            lang: 'pt-br'
+        '/zh/': {
+            lang: 'zh-CN',
+            title: 'VuePress',
+            description: 'Vue 驱动的静态网站生成器'
         },
         '/en/': {
             lang: 'en'
@@ -49,29 +52,57 @@ module.exports = {
         docsDir: '',
         editLinkText: '',
         lastUpdated: false,
+        displayAllHeaders: true,
+        locales: {
+            '/': {
+                selectText: 'Languages',
+                label: 'English',
+                ariaLabel: 'Languages',
+                editLinkText: 'Edit this page on GitHub',
+                serviceWorker: {
+                    updatePopup: {
+                        message: "New content is available.",
+                        buttonText: "Refresh"
+                    }
+                },
+            },
+
+        },
         nav: [{
-                text: 'Guide',
-                link: '/guide/',
+                text: '指南',
+                link: '/zh/guide/',
             },
             {
-                text: 'Config',
-                link: '/config/'
+                text: '版本管理',
+                link: '/zh/git/'
             },
             {
                 text: 'VuePress',
                 link: 'https://v1.vuepress.vuejs.org'
             }
         ],
-        sidebar: {
-            '/guide/': [{
-                title: 'Guide',
-                collapsable: false,
-                children: [
-                    '',
-                    'using-vue',
-                ]
-            }],
-        }
+        sidebar: 'auto',
+        // sidebar: {
+        //     '/zh/guide/': [{
+        //         title: '指南',
+        //         collapsable: false,
+        //         children: [
+        //             '',
+        //             'using-vue'
+        //         ]
+        //     }],
+        //     '/zh/git/': [{
+        //         title: "介绍",
+        //         collapsable: false
+        //     }, {
+        //         title: "指令",
+        //         collapsable: false,
+        //         children: [
+        //             'directive'
+        //         ]
+
+        //     }]
+        // }
     },
 
     /**
@@ -80,6 +111,14 @@ module.exports = {
     plugins: [
         '@vuepress/plugin-back-to-top',
         '@vuepress/plugin-medium-zoom',
+        '@vuepress/pwa', {
+            serviceWorker: true,
+            updatePopup: true
+        }
     ],
+    /**
+     * 配置热更新的文件
+     */
+
 
 }
